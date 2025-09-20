f800c343f390a05110c169c35df0efd1de828b94
ashish11011 <bishnoi11011@gmail.com>
2025-09-20 16:56:45 +0530

    commig soon

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function ComingSoon() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray50 text-gray-700 text-center p-6">
+      <h1 className="text-6xl font-bold mb-4">Coming Soon</h1>
+      <p className="text-lg text-gray-500">
+        Our website is under construction. Please check back soon.
+      </p>
+    </main>
+  );
+}
